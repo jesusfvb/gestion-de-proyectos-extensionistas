b1216be1148e64b039364e445087ac2a980d7fd0
Jesus F. Vazquez Biltre <jesusfvb2@gmail.com>
2021-12-18 18:52:39 -0500

    Barra Superior

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -9,24 +9,34 @@ import E404 from "./paginas/E404";
 export default function App(): ReactElement {
     axios.defaults.baseURL = 'http://localhost:8080';
     const body = document.getElementsByTagName("body")[0]
-    const [sesion, setSesion] = useState<string>("jj")
+    const [session, setSession] = useState<string | null>(localStorage.getItem("jwt"))
 
+    const serrarSession = () => {
+        localStorage.clear()
+        setSession(null)
+    }
 
-    body.style.backgroundImage = `url(${img_2})`;
+    if (session !== null) {
+        body.style.minHeight = "100vh"
+        body.style.background = `linear-gradient(#fff9  , #fff9),url(${img_2})`;
+    } else {
+        body.style.minHeight = "auto"
+        body.style.background = `url(${img_2})`;
+    }
     body.style.backgroundRepeat = "no-repeat";
     body.style.backgroundSize = "cover";
     return (
         <BrowserRouter>
             <Routes>
 
-                {(sesion !== undefined) ?
+                {(session !== null) ?
                     <>
-                        <Route element={<BarraDeNavegacion/>}>
-                            <Route path="/" element={<h1>Jajaja</h1>}/>
+                        <Route element={<BarraDeNavegacion serrarSession={serrarSession}/>}>
+                            <Route path="/" element={<h1>Inicio</h1>}/>
                         </Route>
                         <Route path="*" element={<E404/>}/>
                     </> :
-                    <Route path="*" element={<Login setSesion={setSesion}/>}/>}
+                    <Route path="*" element={<Login setSession={setSession}/>}/>}
             </Routes>
         </BrowserRouter>
     );
